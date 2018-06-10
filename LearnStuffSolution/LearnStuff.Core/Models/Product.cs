7729b49d0a1f47072daa3ccbcffc2ba1130f9d4e
AshtonLayne <ashtonl@exigo.com>
2018-06-09 23:28:01 -0500

    Product Manager Pages

---
+++ LearnStuffSolution/LearnStuff.Core/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnStuff.Core.Models
+{
+    public class Product
+    {
+        public string ProductID { get; set; }
+
+        [StringLength (20)]
+        [DisplayName ("Product Name")]
+        public string ProductName { get; set; }
+        public string ProductDesc { get; set; }
+
+        [Range(0,1000)]
+        public string Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product ()
+        {
+            this.ProductID = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ LearnStuffSolution/LearnStuff.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LearnStuff.Core.Models;
+using LearnStuff.DataAccess.InMemory;
+
+namespace LearnStuff.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductCache context;
+
+        public ProductManagerController()
+        {
+            context = new ProductCache();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create (Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.CommitToMemory();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit (string ID)
+        {
+            Product productToEdit = context.Find(ID);
+
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToEdit);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit (Product product, string ID)
+        {
+            Product productToEdit = context.Find(ID);
+
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+
+                productToEdit.Category = product.Category;
+                productToEdit.ProductDesc = product.ProductDesc;
+                productToEdit.Image = product.Image;
+                productToEdit.ProductName = product.ProductName;
+                productToEdit.Price = product.Price;
+
+                context.CommitToMemory();
+
+                return RedirectToAction("Index");
+
+
+            }
+        }
+
+        public ActionResult Delete(string ID)
+        {
+            Product productToDelete = context.Find(ID);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+            }
+        }
+
+
+
+        [HttpPost]
+        [ActionName ("Delete")]
+        public ActionResult ConfirmDelete (Product product, string ID)
+        {
+            Product productToDelete = context.Find(ID);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(ID);
+                context.CommitToMemory();
+                return RedirectToAction("Index");
+            }
+        }
+ 
+    }
+}
